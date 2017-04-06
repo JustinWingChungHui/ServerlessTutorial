e80c4f7c7ce90859969594f17a4bc2e1bf399f05
Tech Luminaire <info@techluminaire.co.uk>
2017-04-06 21:11:50 +0100

    Tutorial just before http://serverless-stack.com/chapters/deploy-the-apis.html

---
+++ install_babel_webpack.bat
@@ -0,0 +1,14 @@
+REM Install Babel and Webpack 
+REM Add Support for ES6/ES7 JavaScript
+
+npm install --save-dev ^
+    babel-core ^
+    babel-loader ^
+    babel-plugin-transform-runtime ^
+    babel-preset-react-app ^
+    serverless-webpack ^
+    glob ^
+    webpack ^
+    webpack-node-externals
+
+npm install --save babel-runtime
